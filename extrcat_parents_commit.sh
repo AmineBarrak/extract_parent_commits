08f77f7de579cbc5037a073fc91332ef786d7a4b
Amine <aminebarrak@live.com>
2018-01-02 14:04:33 -0500

    making script working with parameters

--- extrcat_parents_commit.sh
+++ extrcat_parents_commit.sh
@@ -3,5 +3,5 @@ do
 
   echo $line| grep -Po 'href="\K.*?(?=")'|cut -d'/' -f 5 ;
 
-done< <(curl -s https://github.com/cloudfoundry/uaa/commit/9373fe935e5fe0926e814517066962d9444bed69 \
+done< <(curl -s https://github.com/$1/commit/$2 \
 |grep "class=\"sha\" data-hotkey=")
